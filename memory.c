2595a39119c62c83f117956f30f3016807e3d937
yosswi414 <yosswi2@gmail.com>
2020-08-15 20:58:44 +0900

    added memory test

---
+++ memory.c
@@ -0,0 +1,9 @@
+#include "memory.h"
+
+unsigned int memtest(unsigned int start, unsigned int end) {
+    char flg486 = 0;
+    unsigned int eflg, cr0;
+
+    // check whether arch is 386 or 486 (or later)
+    
+}
---
+++ memory.h
@@ -0,0 +1,9 @@
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
+
+unsigned int memtest(unsigned int start, unsigned int end);
+
+#define EFLAGS_AC_BIT 0x00040000
+#define CR0_CACHE_DISABLE 0x60000000
+
+#endif
